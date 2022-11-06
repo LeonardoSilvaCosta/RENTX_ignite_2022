676fd804b642dfffc63fab2efde6c22e9cd4e503
leonardocosta <sir.costa@yahoo.com.br>
2022-11-05 22:35:59 -0300

    Altera tabela de usuário

--- src/database/index.ts
+++ src/database/index.ts
@@ -14,5 +14,5 @@ export const AppDataSource = new DataSource({
   logging: false,
   entities: [Category, Specification, User],
   subscribers: [],
-  migrations: ["./src/database/migrations/1667692367429-CreateUsers'.ts"],
+  migrations: ["./src/database/migrations/1667698222725-AlterUserDeleteUsername.ts"],
 })
